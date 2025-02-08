f60c8c9d16b4e500c71d38a650c7013fc52137dd
perret <lilian.perret.cn@outlook.com>
2025-02-08 01:21:51 +0100

    Ajout de l'import React pour Next.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,10 @@
+import React from 'react';
+
 export default function Home() {
-    return (
-      <div>
-        <h1>Bienvenue sur VisuAI</h1>
-        <p>Votre plateforme de génération de visuels IA.</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1>Bienvenue sur VisuAI</h1>
+      <p>Votre plateforme de génération de visuels IA.</p>
+    </div>
+  );
+}
